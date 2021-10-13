ec981b534607ca115a9471c03623d7b55d257228
Aldair Enriquez Duran <aldair_fime@hotmail.com>
2021-10-13 13:24:01 -0500

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Dashboard
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+
+        private static extern IntPtr CreateRoundRectRgn
+            (
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+            );
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+
+            lblTitle.Text = "Dashboard";
+            this.PnlFormLoader.Controls.Clear();
+            frmDashboard frmDashboard_Vrb = new frmDashboard() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
+            frmDashboard_Vrb.FormBorderStyle = FormBorderStyle.None;
+            this.PnlFormLoader.Controls.Add(frmDashboard_Vrb);
+            frmDashboard_Vrb.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDashboard_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnDashboard.Height;
+            pnlNav.Top = btnDashboard.Top;
+            pnlNav.Left = btnDashboard.Left;
+            btnDashboard.BackColor = Color.FromArgb(46,51,73);
+
+            lblTitle.Text = "Dashboard";
+            this.PnlFormLoader.Controls.Clear();
+            frmDashboard frmDashboard_Vrb = new frmDashboard() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
+            frmDashboard_Vrb.FormBorderStyle = FormBorderStyle.None;
+            this.PnlFormLoader.Controls.Add(frmDashboard_Vrb);
+            frmDashboard_Vrb.Show();
+        }
+
+        private void btnAnalytics_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnAnalytics.Height;
+            pnlNav.Top = btnAnalytics.Top;
+            pnlNav.Left = btnAnalytics.Left;
+            btnAnalytics.BackColor = Color.FromArgb(46, 51, 73);
+
+            lblTitle.Text = "Analytics";
+            this.PnlFormLoader.Controls.Clear();
+            frmAnalytics frmDashboard_Vrb = new frmAnalytics() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
+            frmDashboard_Vrb.FormBorderStyle = FormBorderStyle.None;
+            this.PnlFormLoader.Controls.Add(frmDashboard_Vrb);
+            frmDashboard_Vrb.Show();
+        }
+
+        private void btnCalendar_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnCalendar.Height;
+            pnlNav.Top = btnCalendar.Top;
+            pnlNav.Left = btnCalendar.Left;
+            btnCalendar.BackColor = Color.FromArgb(46, 51, 73);
+
+            lblTitle.Text = "Calendar";
+            this.PnlFormLoader.Controls.Clear();
+            frmCalendar frmDashboard_Vrb = new frmCalendar() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
+            frmDashboard_Vrb.FormBorderStyle = FormBorderStyle.None;
+            this.PnlFormLoader.Controls.Add(frmDashboard_Vrb);
+            frmDashboard_Vrb.Show();
+        }
+
+        private void btnContactUs_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnContactUs.Height;
+            pnlNav.Top = btnContactUs.Top;
+            pnlNav.Left = btnContactUs.Left;
+            btnContactUs.BackColor = Color.FromArgb(46, 51, 73);
+
+            lblTitle.Text = "Contact Us";
+            this.PnlFormLoader.Controls.Clear();
+            frmContactUs frmDashboard_Vrb = new frmContactUs() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
+            frmDashboard_Vrb.FormBorderStyle = FormBorderStyle.None;
+            this.PnlFormLoader.Controls.Add(frmDashboard_Vrb);
+            frmDashboard_Vrb.Show();
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnSettings.Height;
+            pnlNav.Top = btnSettings.Top;
+            pnlNav.Left = btnSettings.Left;
+            btnSettings.BackColor = Color.FromArgb(46, 51, 73);
+
+            lblTitle.Text = "Settings";
+            this.PnlFormLoader.Controls.Clear();
+            frmSettings frmDashboard_Vrb = new frmSettings() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
+            frmDashboard_Vrb.FormBorderStyle = FormBorderStyle.None;
+            this.PnlFormLoader.Controls.Add(frmDashboard_Vrb);
+            frmDashboard_Vrb.Show();
+        }
+
+        private void btnDashboard_Leave(object sender, EventArgs e)
+        {
+            btnDashboard.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnAnalytics_Leave(object sender, EventArgs e)
+        {
+            btnAnalytics.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnCalendar_Leave(object sender, EventArgs e)
+        {
+            btnCalendar.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnContactUs_Leave(object sender, EventArgs e)
+        {
+            btnContactUs.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnSettings_Leave(object sender, EventArgs e)
+        {
+            btnSettings.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
